94ff754440a0115cbe33d7af828da2f81b453dbe
Lewis <lewis.kagia@student.moringaschool.com>
2023-09-16 13:45:37 +0300

    Done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,24 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()  
+            return response.text
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+            return None
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body:
+            try:
+                return response.json()
+            except ValueError as e:
+                print(f"Error parsing JSON: {e}")
+                return None
+
